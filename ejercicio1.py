6c50a414d322f52710627233430a9909870c4878
Joao Caicedo <joa.caicedo@duocuc.cl>
2026-06-07 16:06:10 -0400

    Resgistro del Sistema Médico

---
+++ ejercicio1.py
@@ -0,0 +1,41 @@
+especialistas_senior = 0
+residentes_junior = 0
+
+while True:
+    try:
+        cantidad_medicos = int(input("Ingresa la cantidad de médicos a resgistrar: "))
+        if cantidad_medicos > 0:
+            break
+        else:
+            print("¡Registro médico inválido! Ingresa un entero positivo para continuar")
+    except ValueError:
+        print("¡Registro médico inválido! Ingresa un entero positivo para continuar")
+
+for i in range(cantidad_medicos):
+    print(f"\n--- Registro del Médico {i+1} ---")
+
+    while True:
+        nombre = input("Ingresa Nombre Profesional (mín. 6 caracterees, sin espacios): ")
+        if len(nombre) >= 6 and " " not in nombre:
+            break
+        else:
+            print("Nombre inválido. Debe tener al menos 6 caraceteres y no contener espacios")
+
+    while True:
+        try:
+            experiencia = int(input(f"Ingresa años de experiencia de {nombre}: "))
+            if experiencia >= 0:
+                break
+            else:
+                print("¡Error clínico! Ingresa un número entero positivo para la experiencia")
+        except ValueError:
+            print("¡Error clínico! Ingresa un número entero positivo para la experiencia")
+    
+    if experiencia > 5:
+        especialistas_senior += 1
+        print(f"{nombre} ha sido clasificado como Especialista Senior.")
+    else:
+        residentes_junior += 1
+        print(f"{nombre} ha sido clasificado como Residente Junior")
+    
+print(f"\n!El hospital cuenta con {especialistas_senior} Especialistas Senior y {residentes_junior} Residentes Junior! ¡Sistema listo para operar!")
